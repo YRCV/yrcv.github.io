5716e66b50750c374ab50d367e15ed534cfeb5e8
YRCV <yrc@njit.edu>
2026-04-03 20:22:49 -0400

    fix: 24h format

--- js/lab.js
+++ js/lab.js
@@ -367,7 +367,7 @@ function initLabPage() {
             hour: '2-digit',
             minute: '2-digit',
             second: '2-digit',
-            hour12: true
+            hour12: false
         }).formatToParts(new Date());
 
         const hour = parts.find((part) => part.type === 'hour')?.value ?? '00';
